4d542580b9db56273861aadf8945ccd62d577c22
axthur <114745834+axthur@users.noreply.github.com>
2022-10-18 15:56:05 -0300

    Update and rename Compra.java to Venda.java

---
+++ src/main/java/Modelo/Venda.java
@@ -0,0 +1,86 @@
+package Modelo;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author arthu
+ */
+public class Venda {
+    private int id;
+    private int idCliente;
+    private int idVendedor;
+    private double valor;
+    private String dataCompra;
+    private String dataPagamento;
+    private String metodoDePagamento;
+
+    public Venda {
+        this.id = -1;
+        this.idCliente = "";
+        this.idVendedor = "";
+        this.valor = "";
+        this.dataCompra = "";
+        this.dataPagamento = "";
+        this.metodoDePagamento = "";
+    }
+
+    public int getCodigo() {
+        return id;
+    }
+
+    public void setCodigo(int id) {
+        this.id = id;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public int getIdVendedor() {
+        return idVendedor;
+    }
+
+    public void setIdVendedor(int idVendedor) {
+        this.idVendedor = idVendedor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getDataCompra() {
+        return dataCompra;
+    }
+
+    public void setDataCompra(String dataCompra) {
+        this.dataCompra = dataCompra;
+    }
+
+    public String getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento(String dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+
+    public String getMetodoDePagamento() {
+        return metodoDePagamento;
+    }
+
+    public void setMetodoDePagamento(String metodoDePagamento) {
+        this.metodoDePagamento = metodoDePagamento;
+    }
+}
