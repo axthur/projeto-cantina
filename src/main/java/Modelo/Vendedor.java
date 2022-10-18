66ee79ca54c260a5b2663569ef00921e6ce2274c
axthur <114745834+axthur@users.noreply.github.com>
2022-10-18 16:00:18 -0300

    Update Vendedor.java

--- src/main/java/Modelo/Vendedor.java
+++ src/main/java/Modelo/Vendedor.java
@@ -12,4 +12,66 @@ package Modelo;
 public class Vendedor {
     private int id;
     private String nome;
+    private String telefone;    
+    private String email;
+    private String endereco;
+    private int cargaHoraria;
+
+    public Vendedor {
+        this.id = -1;
+        this.nome = "";
+        this.telefone = "";
+        this.email = "";
+        this.endereco = "";
+        this.cargaHoraria = "";
+    }
+
+    public int getCodigo() {
+        return id;
+    }
+
+    public void setCodigo(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+    
 }
