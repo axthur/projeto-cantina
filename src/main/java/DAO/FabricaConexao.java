61ea89c0e07b5ca86d4f8a7adcdb042990902fa2
axthur <114745834+axthur@users.noreply.github.com>
2022-10-20 15:35:51 -0300

    Update FabricaConexao.java

--- src/main/java/DAO/FabricaConexao.java
+++ src/main/java/DAO/FabricaConexao.java
@@ -1,17 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DAO;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author arthu
- */
 public class FabricaConexao {
     public static Connection getConnection(){
         Connection Conexao = null;
