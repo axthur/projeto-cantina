72e87111da9c5f64fb529171273a8528e8500212
axthur <114745834+axthur@users.noreply.github.com>
2022-10-20 11:51:26 -0300

    Update Cliente.java

--- src/main/java/Modelo/Cliente.java
+++ src/main/java/Modelo/Cliente.java
@@ -2,15 +2,6 @@ package Modelo;
 
 import DAO.ClienteDAO;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author arthu
- */
 public class Cliente {
     private int id;
     private String nome;
